4a1c37fe4c9a7d5210658e9cffaac1058af67816
richard-to <gohn76@gmail.com>
2013-07-10 01:23:16 -0800

    style(main): Clean up formatting in main sample

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -23,24 +23,25 @@ var TestComposeView = Backbone.View.extend({
 
 var app = new Knockoff.App(".ko-app");
 
-app.module('testServices', [], '.ko-test-module');
-app.module('testServices').value('TestModel', TestModel)
-.value('TestList', TestList)
-.value('ListView', Knockoff.View.List)
-.value('ComposeView', TestComposeView);
+app.module('testServices', [], '.ko-test-module')
+    .value('TestModel', TestModel)
+    .value('TestList', TestList)
+    .value('ListView', Knockoff.View.List)
+    .value('ComposeView', TestComposeView);
 
-var testModule = app.module('testModule', ['testServices'], '.ko-test-module');
-testModule.controller('testController', function(env, TestList, ListView, ComposeView) {
-    var testList = new TestList();
-    testList.add(new testList.model({name: 'Test'}));
 
-    var msgView = new ListView({collection: testList});
-    var composeView = new ComposeView({collection: testList});
+app.module('testModule', ['testServices'], '.ko-test-module')
+    .controller('testController', function(env, TestList, ListView, ComposeView) {
+        var testList = new TestList();
+        testList.add(new testList.model({name: 'Test'}));
 
-    env.testList = testList;
-    env.$el.find(".ko-listview").html(msgView.render().el);
-    env.$el.find(".ko-composeview").html(composeView.render().el);
-})
-.run(function(controller) {
-    controller('testController');
-});
+        var msgView = new ListView({collection: testList});
+        var composeView = new ComposeView({collection: testList});
+
+        env.testList = testList;
+        env.$el.find(".ko-listview").html(msgView.render().el);
+        env.$el.find(".ko-composeview").html(composeView.render().el);
+    })
+    .run(function(controller) {
+        controller('testController');
+    });

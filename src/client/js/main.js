3af3c10a41804cc330b76063cdd06dba3bd47e20
richard-to <gohn76@gmail.com>
2013-07-10 05:12:57 -0800

    feat(main): Add sample home view

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -9,14 +9,36 @@ var MsgList = Backbone.Collection.extend({
     model: MsgModel
 });
 
-var MsgComposeView = Backbone.View.extend({
+var HomeView = knockoff.ui.View.extend({
+    propList: ['router'],
+    tagName: 'div',
+    template: _.template($("#ko-home-tmpl").html()),
+    initialize: function() {
+        _.bindAll(this, 'render', 'link');
+    },
+    events: {
+        'click .ko-link': 'link'
+    },
+    render: function() {
+        this.$el.html(this.template());
+        return this;
+    },
+    link: function() {
+        this.router.navigate("help", {trigger: true});
+        return false;
+    }
+});
+
+var MsgComposeView = knockoff.ui.View.extend({
+    propList: ['router'],
     tagName: 'div',
     template: _.template($("#ko-composeview-tmpl").html()),
     initialize: function() {
-        _.bindAll(this, 'render', 'submit');
+        _.bindAll(this, 'render', 'submit', 'link');
     },
     events: {
-        'click .ko-submit': 'submit'
+        'click .ko-submit': 'submit',
+        'click .ko-link': 'link'
     },
     render: function() {
         this.$el.html(this.template());
@@ -26,6 +48,10 @@ var MsgComposeView = Backbone.View.extend({
         this.collection.add(new this.collection.model({
             msg: this.$el.find('.ko-text').val()
         }));
+        return false;
+    },
+    link: function() {
+        this.router.navigate('', {trigger: true});
         return false;
     }
 });
@@ -47,9 +73,20 @@ knockoff.module('msgModule', ['msgServices'])
         env.msgList = msgList;
 
         var msgView = new ListView({collection: env.msgList});
-        var composeView = new MsgComposeView({collection: env.msgList});
+        var composeView = new MsgComposeView({collection: env.msgList}, ['router']);
         env.$el.find(".ko-listview").html(msgView.render().el);
         env.$el.find(".ko-composeview").html(composeView.render().el);
+    });
+
+knockoff.module('homeModule')
+    .el('.ko-test-module')
+    .value('HomeView', HomeView)
+    .config(function(routerProvider) {
+        routerProvider.add('', 'home', 'homeController');
+    })
+    .controller('homeController', function(env, HomeView) {
+        var homeView = new HomeView({}, ['router']);
+        env.$el.find(".ko-homeview").html(homeView.render().el);
     });
 
 Backbone.history.start();

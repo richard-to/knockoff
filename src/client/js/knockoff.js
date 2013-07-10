fb70f77c58c71a32cedc9b2572ed0f5365bab471
richard-to <gohn76@gmail.com>
2013-07-10 00:02:08 -0800

    refactor(views): Move views to separate file

--- src/client/js/knockoff.js
+++ src/client/js/knockoff.js
@@ -1,9 +1,5 @@
 (function(window, undefined) {
 
-    var MixinConfigureProps = function(options, propList) {
-        if (this.options) options = _.extend({}, _.result(this, 'options'), options);
-        _.extend(this, _.pick(options, propList));
-    };
 
     var Injector = function(providerCache, providerSuffix) {
         this.depsRegExp = new RegExp(/\((.*)\) *\{/);
@@ -230,60 +226,9 @@
         return moduleService(name);
     };
 
-    var ListItemView = Backbone.View.extend({
-        tagName: 'li',
-        template: _.template($("#ko-listitemview-tmpl").html()),
-        initialize: function() {
-            _.bindAll(this, 'render');
-        },
-        render: function() {
-            this.$el.html(this.template(this.model.attributes));
-            return this;
-        }
-    });
+    var Knockoff = {App: App};
 
-    var ListView = function(options) {
-        var propList = ['itemView'];
-        options = options || {};
-        this._configureProps(options || {}, propList);
-        Backbone.View.apply(this, [options]);
-    };
-
-    _.extend(ListView.prototype, Backbone.View.prototype, {
-        tagName: 'ul',
-        itemView: ListItemView,
-        initialize: function() {
-            _.bindAll(this, 'render', 'appendItem');
-            this.collection.on('add', this.appendItem);
-        },
-        render: function() {
-            var self = this;
-            _(this.collection.models).each(function(item) {
-                self.appendItem(item);
-            }, this);
-            return this;
-        },
-        appendItem: function(item) {
-            var view = new this.itemView({
-                model: item
-            });
-            this.$el.append(view.render().el);
-        },
-        _configureProps: MixinConfigureProps
-    });
-    ListView.extend = Backbone.View.extend;
-
-    var View = {
-        List: ListView,
-        ListItem: ListItemView
-    };
-
-    var Knockoff = {
-        App: App,
-        View: View
-    };
     window.Knockoff = Knockoff;
-
     if (typeof define === "function" && define.amd) {
         define("Knockoff", [], function() { return Knockoff; });
     }
